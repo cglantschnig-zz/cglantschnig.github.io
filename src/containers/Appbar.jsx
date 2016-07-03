b76af57aecf28ece03665623226c7a23f26b261f
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2016-07-03 20:37:24 +0200

    changed appbar

--- src/containers/Appbar.jsx
+++ src/containers/Appbar.jsx
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from "react";
 
-import {Nav, NavbarBrand, Navbar, NavItem, CollapsibleNav} from "react-bootstrap";
+import {Nav, NavbarBrand, Navbar, NavItem, CollapsibleNav, Container} from "react-bootstrap";
 
 import {Link} from "react-router";
 
@@ -14,8 +14,7 @@ import {FormattedMessage} from "react-intl";
 
 import connectRouter from "../decorators/connectRouter";
 
-const LOGOUT_EVENT_KEY = "LOGOUT";
-const SIDEBAR_EVENT_KEY = "SIDEBAR";
+const logo = require("../assets/img/glantschnig-pro-top.png");
 
 @connectRouter
 @connect()
@@ -32,13 +31,19 @@ export default class Appbar extends Component {
     const {router} = this.props;
 
     return (
-      <div styleName="Appbar">
-        <Navbar fluid fixedTop>
-          <NavbarBrand>
-            <Link styleName="Appbar-brand" to="/">
-              <aside><FormattedMessage id="title"/></aside>
-            </Link>
-          </NavbarBrand>
+      <div styleName="Appbar" className="appbar">
+        <Navbar fixedTop>
+            <NavbarBrand>
+              <Link styleName="Appbar-brand" to="/">
+                <aside><img src={logo} /></aside>
+              </Link>
+            </NavbarBrand>
+            <Navbar.Collapse>
+              <Nav pullRight>
+                <NavItem eventKey={1} styleName="navbar-link-item" href="https://github.com/cglantschnig" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></NavItem>
+                <NavItem eventKey={2} styleName="navbar-link-item" href="http://stackoverflow.com/users/1164646/safari" target="_blank"><i className="fa fa-stack-overflow" aria-hidden="true"></i></NavItem>
+              </Nav>
+            </Navbar.Collapse>
         </Navbar>
       </div>
     );
