c7b537856d79adf8cb1b35b512d19202e209991c
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2016-07-03 16:53:44 +0200

    production version on public folder

--- config/env/env-development.sh
+++
@@ -1,6 +0,0 @@
-export NODE_ENV=development
-export BABEL_ENV=development
-
-export API_BASE_URL=http://10.0.0.10:8080
-
-export PUBLIC_ABSOLUTE_URL_PATH_PREFIX=/ # set for nginx to the dist directory (local http-server auto cd.. into it)
